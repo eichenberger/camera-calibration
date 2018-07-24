64f0bd9f83e8606b36c620187056fb0473c3bb59
Stefan Eichenberger <eichest@gmail.com>
2018-07-24 16:57:48 +0200

    cleanup

--- tests/extensions/orb/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup, Extension
-
-module1 = Extension('orb',
-                    sources = ['orbmodule.c'])
-
-setup (name = 'Orb',
-       version = '1.0',
-       description = 'This is a orb package',
-       ext_modules = [module1])
--- tests/test_mayavi.py
+++
@@ -1,9 +0,0 @@
-# Author: Prabhu Ramachandran <prabhu at aero dot iitb dot ac dot in>
-# Copyright (c) 2007, Enthought, Inc.
-# License: BSD style.
-import numpy as np
-from mayavi import mlab
-
-x, y, z = np.random.randn(3, 100)
-mlab.points3d(x, y, z)
-mlab.show()
