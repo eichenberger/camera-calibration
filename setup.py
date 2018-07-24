20ffe42f0446d0d4466df54bbe829735ffcb25ff
Stefan Eichenberger <eichest@gmail.com>
2018-07-24 17:11:11 +0200

    setup.py: add setup file

---
+++ setup.py
@@ -0,0 +1,16 @@
+import os
+from setuptools import setup
+setup(
+    name = "camera-calibration",
+    version = "1.0.0",
+    author = "Stefan Eichenberger",
+    author_email = "eichest@gmail.com",
+    description = ("An example on how to calibrate a camera based on a 3D cloud"),
+    license = "MIT",
+    keywords = "camera-calibration, computer vision",
+    install_requires=[
+        'numpy >= 1.14.1',
+        'scipy >= 0.19.1',
+        'opencv >= 3.2.0'
+    ]
+)
